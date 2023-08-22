08b57bddc0fb1e899f852aa5040ca485b4ac25ac
Adam Bishop <adam.michael.bishop@gmail.com>
2023-08-22 11:52:36 -0500

    Added http handlers for submitting a form

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import("fmt")
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err !=nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	if r.URL.Path != "/form" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	fmt.Fprintf(w, "Name: %s, Email: %s", name, email)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+}
 
 func main() {
-	fmt.Println("yo")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form/submit", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting server on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
